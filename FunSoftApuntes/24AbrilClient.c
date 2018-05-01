bca62814c660043bbb94658badbe1bd75760084e
MMarinov97 <marinovantonov@gmail.com>
2018-05-01 18:05:41 +0200

    Borrados archivos que sobran

--- FunSoftApuntes/24AbrilClient.c
+++
@@ -1,6 +0,0 @@
-int main(int argc, char *argv[]){
-    // Procesamos argumentos: fifo para escritura
-    // Abriro FIFO para escribir
-    // Iteramos hasta que el usuario introduce "fin"
-    // Cerramos la FIFO
-}
